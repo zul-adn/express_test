fbabcb293ea4100033d324f4eb3536c7825b4079
zul-adn <zbojetx@gmail.com>
2024-07-22 14:41:39 +0700

    fix: compose

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const app = express();
 const port = 5000;
 
 app.get("/all", (req, res) => {
-  try {
-    let response = [];
-    fetch("https://dummyjson.com/users")
-      .then((resp) => resp.json())
-      .then((json) => res.send(json));
-  } catch (error) {
-    res.send("something hapend!! please check your datadoc");
-    throw Error;
-  }
+  res.send("API for user service");
+  // try {
+  //   let response = [];
+  //   fetch("https://dummyjson.com/users")
+  //     .then((resp) => resp.json())
+  //     .then((json) => res.send(json));
+  // } catch (error) {
+  //   res.send("something hapend!! please check your datadoc");
+  //   throw Error;
+  // }
 });
 
 app.listen(port, () => {
