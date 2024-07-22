c48b76f0ce434e53448fe45fc9b10d409583afb3
zul-adn <zbojetx@gmail.com>
2024-07-22 14:11:43 +0700

    feat: add response

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
+app.get("/all", async (req, res) => {
+  try {
+    console.log("res");
+    await fetch("https://dummyjson.com/users")
+      .then((resp) => resp.json())
+      .then((json) => res.json(json));
+  } catch (error) {
+    throw Error;
+  }
 });
 
 app.listen(port, () => {
